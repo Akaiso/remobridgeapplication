ea509892189931c1eaee410f72c85cd6ccf2f709
Akaiso <akaisobenson@yahoo.com>
2025-04-26 07:51:23 +0100

    adding .env to pubspec.yaml to assets and then git ignoring it as well

---
+++ .env.dart
@@ -0,0 +1,7 @@
+APIKEY=AIzaSyCcEKYVTRhShTPy4ygXZ5yjlXkk3kU2ONM
+AUTHDOMAIN=remobridgeapplication.firebaseapp.com
+PROJECTID=remobridgeapplication
+STORAGEBUCKET=remobridgeapplication.firebasestorage.app
+MESSAGINGSENDERID=298456778897
+APPID=1:298456778897:web:a851de03ca8213c269d730
+MEASUREMENTID=G-3XH0PN1GTS
